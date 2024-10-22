022aacbb195f18d0ff41800a9b1cab34d25f8f04
ZhuAnson-ICD201-01 <anson.zhu@student.tdsb.on.ca>
2024-10-22 03:31:53 +0000

    asdasd

--- PP2_3.py
+++ PP2_3.py
@@ -1,12 +1,37 @@
 
 
 def q1(): 
-  #Write Assignment code here
-
+  word = input("Enter a word: ")
+  if word[-3:] == "ife":
+    print("-ives")
+  elif word[-2:] == "ey":
+    print("-eys")
+  elif word[-1:] == "y":
+    print("-ies")
+  else:
+    print("-s")
 
 def q2(): 
-  #Write Assignment code here
+  num = int(input("Input an integer:"))
+  if num > 0:
+    print(f"{num} is positive")
+  elif num < 0:
+    print(f"{num} is negative")
 
+def q3():
+  side1 = float(input("Input a side length: "))
+  side2 = float(input("Input a side length: "))
+  side3 = float(input("Input a side length: "))
+  if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1): 
+   if side1 == side2 == side3:
+      print("Equilateral")
+    elif side1 == side2 or side1 == side3 or side2 == side3:
+      print("Isosceles")
+   elif
+      print("Scalene")
+  else:
+    print("No Triangle")
+  
 
 
 #Do not alter the following code
@@ -14,3 +39,4 @@ def q2():
 
 q1()
 q2()
+q3()
